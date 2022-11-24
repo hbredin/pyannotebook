e68b5889e44a7a4ab1b7f34ed680a0a239e2bc5d
Hervé BREDIN <hbredin@users.noreply.github.com>
2022-11-24 16:45:03 +0100

    feat(annotation): add regions traitlet

--- pyannotebook/annotation.py
+++ pyannotebook/annotation.py
@@ -1,8 +1,17 @@
 import ipywidgets
 import traitlets
-from typing import Optional, Union
-from pyannote.core import Annotation
+from typing import Dict, Optional, Union
+from pyannote.core import Annotation, Segment
 from itertools import filterfalse, count
+import random
+import string
+from functools import partial
+
+def get_annotation(regions, labels):
+    annotation = Annotation()
+    for region in regions:
+        annotation[Segment(region["start"], region["end"]), region["id"]] = labels.get(region["label"], region["label"])
+    return annotation
 
 
 class AnnotationWidget(ipywidgets.Widget):
@@ -21,34 +30,54 @@ class AnnotationWidget(ipywidgets.Widget):
     Traitlets
     ---------
     labels : int -> human-readable
+    regions : list of {"start": float, "end": float, "id": str, "label": int}
     """
 
     labels = traitlets.Dict().tag(sync=True)
+    regions = traitlets.List().tag(sync=True)
 
     def __init__(self, annotation: Optional[Annotation] = None):
         super().__init__()
         self.annotation = annotation
-            
-    def get_annotation(self):
-        return self._annotation
+    
+    def _get_annotation(self):
+        return get_annotation(self.regions, self.labels)
 
-    def set_annotation(self, annotation: Union[Annotation, None]):
+    annotation = property(_get_annotation)
 
-        if annotation is None:
-            self._annotation = Annotation()
-            self.labels = dict()
+    @traitlets.observe("labels")
+    def labels_has_changed(self, change: Dict):
+        self.slebal = {label: idx for idx, label in change["new"].items()}
+
+    @traitlets.observe("regions")
+    def regions_has_changed(self, change: Dict):
+        added_labels = set(region["label"] for region in change["new"]) - set(self.labels)
+        if added_labels:
+            new_labels = dict(self.labels)
+            for label in added_labels:
+                new_labels[label] = label
+            self.labels = new_labels
+
+    # def _set_annotation(self, annotation: Union[Annotation, None]):
+
+    #     if annotation is None:
+    #         annotation = Annotation()
+    #         self.labels = dict()
         
-        else:
-            added_labels = set(annotation.labels()) - set(self.labels.values())
-            print(f"{added_labels=}")
-            if added_labels:
-                new_labels = dict(self.labels)
-                index_pool = filterfalse(lambda i: i in self.labels, count(start=1))
-                for label in added_labels:
-                    index = next(index_pool)
-                    new_labels[index] = label
-                self.labels = new_labels
-            self._annotation = annotation
-
-    annotation = property(get_annotation, set_annotation)
-
+    #     else:
+    #         added_labels = set(annotation.labels()) - set(self.labels.values())
+    #         if added_labels:
+    #             new_labels = dict(self.labels)
+    #             index_pool = filterfalse(lambda i: i in self.labels, count(start=1))
+    #             for label in added_labels:
+    #                 index = next(index_pool)
+    #                 new_labels[index] = label
+    #             self.labels = new_labels
+        
+    #     self.regions = [
+    #         {
+    #             "start": segment.start, 
+    #             "end": segment.end, 
+    #             "label": self.slebal[label],
+    #             "id": "".join(random.choices(string.ascii_lowercase, k=20))
+    #         } for segment, _, label in annotation.itertracks(yield_label=True)]
